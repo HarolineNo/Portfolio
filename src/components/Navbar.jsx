d72c47ae8d9ad6710d4157a876fccb2ad0354035
harolinenoesi <harolinenoesi@gmail.com>
2024-11-13 21:41:03 -0500

    created new vite project instead and starting navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import '../components/styles/Navbar.css';
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>About</li>
+                <li>Projects</li>
+            </ul>
+        </nav>
+    );
+}
