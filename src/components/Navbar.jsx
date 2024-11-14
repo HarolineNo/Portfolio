6458a49444b6d25914532baa6983314eaed85502
harolinenoesi <harolinenoesi@gmail.com>
2024-11-13 21:59:17 -0500

    added router to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
 import '../components/styles/Navbar.css';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
         <nav className="navbar">
             <ul>
-                <li>About</li>
-                <li>Projects</li>
+                <li>
+                    <Link to="">Haroline</Link>
+                </li>
+                <li>
+                    <Link to="/About">About</Link>
+                </li>
+                <li>
+                    <Link to="/Projects">Projects</Link>
+                </li>
+                <li>
+                    <Link to="/Contact">Contact</Link>
+                </li>
             </ul>
         </nav>
     );
