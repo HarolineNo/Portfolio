4301579e98bc9537d7c46826765297804664b101
harolinenoesi <harolinenoesi@gmail.com>
2024-11-14 18:21:06 -0500

    started projects page

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import data from "../../data/index.json";
 
 export const Projects = () => {
   return (
-    <div>Projects</div>
-  )
+    <section>
+      <div>
+        <div>
+          <h2>My Projects</h2>
+        </div>
+        <div>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 33 33" fill="none" />
+            Visit My GitHub
+          </button>
+        </div>
+      </div>
+      <div>
+        {data?.projects?.map((item, index) => (
+          <div key={index}>
+            <div>
+              <img src={item.src} alt="Placeholder" />
+            </div>
+            <div>
+              <div>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+              </div>
+              <p>
+                {item.link}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 20 19" fill="none" />
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
