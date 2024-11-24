4770c8d5f7937b80928da84e376bc026b0636475
harolinenoesi <harolinenoesi@gmail.com>
2024-11-24 18:18:38 -0500

    added pictures for skills

--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -1,14 +1,50 @@
-import data from '../../data/index.json';
 import '../styles/Skills.css';
 
 export const Skills = () => {
   return (
     <div className="skills-section">
       <h1>Skills</h1>
-      <div className="skills-list">
-        {data.skills.map((skill, index) => (
-          <div key={index}>{skill.title}</div>
-        ))}
+      <div className="skills-list1">
+        <div className="skill">
+          <img src='src\assets\images\html-5.png' alt='HTML' className="skill-img" />
+        </div>
+        <div className="skill">
+          <img src='src\assets\images\css3.png' alt='css3' className="skill-img" />
+        </div>
+        <div className="skill">
+          <img src='src\assets\images\js.png' alt='javascript' className="skill-img" />
+        </div>
+        <div className="skill">
+          <img src='src\assets\images\python.png' alt='python' className="skill-img" />
+        </div>
+      </div>
+      <div className="skills-list2">
+        <div className="skill">
+          <img src='src\assets\images\java.png' alt='java' className="skill-img" />
+        </div>
+        <div className="skill">
+          <img src='src\assets\images\csharp.png' alt='c sharp' className="skill-img" />
+        </div>
+        <div className="skill">
+          <img src='src\assets\images\science.png' alt='react' className="skill-img" />
+        </div>
+        <div className="skill">
+          <img src='src\assets\images\django.png' alt='django' className="skill-img" />
+        </div>
+      </div>
+      <div className="skills-list3">
+        <div className="skill">
+          <img src='src\assets\images\mysql.png' alt='mySQL' className="skill-img" />
+        </div>
+        <div className="skill">
+          <img src='src\assets\images\git.png' alt='git' className="skill-img" />
+        </div>
+        <div className="skill">
+          <img src='src\assets\images\bootstrap.png' alt='bootstrap' className="skill-img" />
+        </div>
+        <div className="skill">
+          <img src='src\assets\images\unity.png' alt='unity' className="skill-img" />
+        </div>
       </div>
     </div>
   )
