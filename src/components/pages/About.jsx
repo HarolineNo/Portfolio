151d3866f65cb13b61f2d5c46767893cfba8840a
harolinenoesi <harolinenoesi@gmail.com>
2024-11-23 22:38:57 -0500

    fixed padding issues AGAIN!

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,16 +2,18 @@ import '../styles/About.css';
 
 export const About = () => {
   return (
-    <div>
+    <div className="about-section">
       <div>
         <h1 className="title">About</h1>
         <div className="title-box"></div>
       </div>
-      <div className="container">
+      <div className="about-container">
         <p className="about">
           Computer Science second-year student with a foundation in programming languages such as Python and Java, an interest in all-things tech and seeking internships in software engineering. Developed full-stack projects utilizing tools like React Native and Django, currently building projects that utilize other languages and frameworks. Eager to apply and expand technical expertise in real-world applications and planning on future projects in robotics. 
         </p>
-        <img src='src\assets\images\quiz.png' />
+        <div className="about-image">
+          <img src='src\assets\images\quiz.png' />
+        </div>
       </div>
     </div>
   )
