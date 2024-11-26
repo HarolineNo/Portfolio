1bd5c8a3b01fbe90dc4d7c4ef7ebd75668934a19
harolinenoesi <harolinenoesi@gmail.com>
2024-11-25 19:05:12 -0500

    changed color of cards and it's buttons

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import Card from "react-bootstrap/Card";
+import Button from 'react-bootstrap/Button';
 import data from "../../data/index.json";
 import "../styles/Projects.css";
 
 export const Projects = () => {
   const [positionIndexes, setPositionIndexes] = useState([0, 1, 2]);
-  const viewportWidth = useState(window.innerWidth);
-
-  const centerX = viewportWidth / 2;
-  console.log(centerX);
 
   const handleNext = () => {
     setPositionIndexes((prevIndexes) =>
@@ -30,8 +27,8 @@ export const Projects = () => {
 
   const cardVariants = {
     center: { x: "-50%", scale: 1, zIndex: 5, opacity: 1 },
-    left: { x: "20%", scale: 0.8, zIndex: 2, opacity: 0.5 },
-    right: { x: "-120%", scale: 0.8, zIndex: 2, opacity: 0.5 },
+    left: { x: "30%", scale: 0.8, zIndex: 2, opacity: 0.5 },
+    right: { x: "-130%", scale: 0.8, zIndex: 2, opacity: 0.5 },
   };
 
   return (
@@ -46,19 +43,21 @@ export const Projects = () => {
             variants={cardVariants}
             transition={{ duration: 0.5 }}
             className="project-card">
-            <Card>
+            <Card className="cards">
               <Card.Img
                 variant="top"
                 src={item.src}
                 alt={item.title} 
-                style={{ height: "10rem", objectFit: "cover" }} 
+                style={{ height: "20rem", borderRadius: "10px", padding: "10px" }} 
               />
               <Card.Body>
                 <Card.Title>{item.title}</Card.Title>
-                <Card.Text style={{ fontStyle: "italic" }}>{item.tech}</Card.Text>
+                <Card.Text style={{ fontStyle: "italic", color: "rgb(60, 60, 60)" }}>{item.tech}</Card.Text>
                 <Card.Text>{item.description}</Card.Text>
-                <Card.Link href={item.demoLink} target="_blank" rel="noopener noreferrer">Demo</Card.Link>
-                <Card.Link href={item.link} target="_blank" rel="noopener noreferrer">Source Code</Card.Link>
+                <div className="card-btn-container">
+                  <Button href={item.demoLink} target="_blank" rel="noopener noreferrer" className="card-btn">Demo</Button>
+                  <Button href={item.link} target="_blank" rel="noopener noreferrer" className="card-btn">Code</Button>
+                </div>
               </Card.Body>
             </Card>
           </motion.div>
