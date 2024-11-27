93775063c832626850e1ac6be9fee86843949d84
harolinenoesi <harolinenoesi@gmail.com>
2024-11-27 01:28:18 -0500

    added bg for projects and different pic for about

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -36,7 +36,7 @@ export const Projects = () => {
                 variant="top"
                 src={item.src}
                 alt={item.title}
-                style={{ height: "20em", borderTopLeftRadius: "30px", borderTopRightRadius: "30px" }} />
+                style={{ height: "22em", borderRadius: "30px", padding: "1rem" }} />
               <Card.Body>
                 <Card.Title style={{ color: "white" }}>{item.title}</Card.Title>
                 <Card.Text style={{ fontStyle: "italic", color: "white" }}>{item.tech}</Card.Text>
