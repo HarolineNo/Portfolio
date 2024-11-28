d9ce6e0ccce0c67fc253fe0a40d3791b930c554a
harolinenoesi <harolinenoesi@gmail.com>
2024-11-28 00:46:35 -0500

    footer and min-projects responsive

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -16,23 +16,25 @@ export const Contact = () => {
           </a>
         </div>
       </div>
-      <div className="contact-info">
-        <div className="contact-container">
-          <h5 className="contact-title">Contact</h5>
-          <p className="phone">(973) 771-9969</p>
-          <p className="email1">noesih1@montclair.edu</p>
-          <p className="email2">harolinenoesi@gmail.com</p>
+      <div className="footer-footer">
+        <div className="contact-info">
+          <div className="contact-container">
+            <h5 className="contact-title">Contact</h5>
+            <p className="phone">(973) 771-9969</p>
+            <p className="email1">noesih1@montclair.edu</p>
+            <p className="email2">harolinenoesi@gmail.com</p>
+          </div>
         </div>
+        <Navbar data-bs-theme="dark" className="footer-navbar">
+          <Nav className="link-list">
+            <h5 className="links-title">Menu</h5>
+            <Nav.Link href="#Home" style={{paddingLeft: 0, color: 'white', alignContent: 'center'}}>Home</Nav.Link>
+            <Nav.Link href="#About" style={{paddingLeft: 0, color: 'white'}}>About</Nav.Link>
+            <Nav.Link href="#Skills" style={{paddingLeft: 0, color: 'white'}}>Skills</Nav.Link>
+            <Nav.Link href="#Projects" style={{paddingLeft: 0, color: 'white'}}>Projects</Nav.Link>
+          </Nav>
+        </Navbar>
       </div>
-      <Navbar data-bs-theme="dark" className="footer-navbar">
-        <Nav className="link-list">
-          <h5 className="links-title">Menu</h5>
-          <Nav.Link href="#Home" style={{paddingLeft: 0, color: 'white'}}>Home</Nav.Link>
-          <Nav.Link href="#About" style={{paddingLeft: 0, color: 'white'}}>About</Nav.Link>
-          <Nav.Link href="#Skills" style={{paddingLeft: 0, color: 'white'}}>Skills</Nav.Link>
-          <Nav.Link href="#Projects" style={{paddingLeft: 0, color: 'white'}}>Projects</Nav.Link>
-        </Nav>
-      </Navbar>
     </div>
   )
 }
